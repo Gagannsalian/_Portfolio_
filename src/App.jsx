8eb27d29cf8de8689b7f94073cc73a5fc78f276d
gagannsalian <saliangagan00@gmail.com>
2025-01-31 22:44:15 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Change here
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
